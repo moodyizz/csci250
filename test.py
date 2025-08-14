a8fc546b89940b77427844d7e44af51f8163d6e7
moodyiz <moodyiz@protonmail.com>
2025-08-14 19:37:07 +0200

    Add test.py for week0's lesson.

---
+++ test.py
@@ -0,0 +1,10 @@
+print("This is a sample print as per instructions of the professor: ChatGPT ;)")
+print("The second print on a new line.")
+print("...and the third line.")
+
+print("""
+Printing now a multiple line,
+with new lines every line,
+as can be seen by reading this.
+Good luck with the course csci250!
+""")
